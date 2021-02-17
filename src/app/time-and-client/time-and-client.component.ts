1a14c150eec20b17d8463a69c25fd1689579f2ed
Kevin Zhu <kevinzhu822@gmail.com>
2021-02-17 05:03:05 -0800

    small fixes here and there. also added current time button

--- src/app/time-and-client/time-and-client.component.ts
+++ src/app/time-and-client/time-and-client.component.ts
@@ -9,14 +9,36 @@ import { SubmissionService } from '../submission.service';
 export class TimeAndClientComponent implements OnInit {
   public inputTime: string;
   public inputClientName: string;
+  public currentTime: string;
   
   constructor(private submissionService: SubmissionService) { }
-
-  ngOnInit() {
+  
+  ngOnInit(): void {
+    this.currentTime = this.getTimeNow();
   }
+
+  onSelectCurrentTime() {
+    this.inputTime = this.currentTime;
+    this.timeChange();
+  }
+
 
   timeChange() {
     this.submissionService.setField("time", this.inputTime, false, null);
+  }
+
+  getTimeNow() {
+    var current = new Date();
+    const hours = (current.getHours()%12).toString();
+    var minutes = current.getMinutes().toString();
+    if (minutes.length == 1) {
+      minutes = "0"+minutes;
+    }
+    var hoursMinutes = hours + ":" + minutes;
+    var time = current.toLocaleTimeString();
+    var suffix = time.substring(time.length-2, time.length);
+    return hoursMinutes+" "+suffix;
+    
   }
 
   clientChange() {
